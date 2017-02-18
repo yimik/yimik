b5c9417c7bce445c4d3738405f92df1daf17bf6d
yimik <398075986@163.com>
2017-02-18 20:27:57 +0800

    change header css like http://spring.io

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 项目简介
 - 本项目为wordpress主题
+- 项目基于开源主题[bones](https://github.com/eddiemachado/bones)开发
 - 目前进度开发中，敬请期待
--- header.php
+++ header.php
@@ -49,7 +49,7 @@
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<p id="logo" class="" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
