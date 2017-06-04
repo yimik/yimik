c42b5a150a0e75a112dbf49e861ae0ed65b564ae
yimik <398075986@163.com>
2017-06-04 22:08:49 +0800

    remove post types

--- library/bones.php
+++ library/bones.php
@@ -198,19 +198,19 @@ function bones_theme_support() {
 	// to add header image support go here: http://themble.com/support/adding-header-background-image-support/
 
 	// adding post format support
-	add_theme_support( 'post-formats',
-		array(
-			'aside',             // title less blurb
-			'gallery',           // gallery of images
-			'link',              // quick link to other site
-			'image',             // an image
-			'quote',             // a quick quote
-			'status',            // a Facebook like status update
-			'video',             // video
-			'audio',             // audio
-			'chat'               // chat transcript
-		)
-	);
+//	add_theme_support( 'post-formats',
+//		array(
+//			'aside',             // title less blurb
+//			'gallery',           // gallery of images
+//			'link',              // quick link to other site
+//			'image',             // an image
+//			'quote',             // a quick quote
+//			'status',            // a Facebook like status update
+//			'video',             // video
+//			'audio',             // audio
+//			'chat'               // chat transcript
+//		)
+//	);
 
 	// wp menus
 	add_theme_support( 'menus' );
