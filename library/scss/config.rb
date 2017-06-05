7873f5b3cea9c1fca463183ec9c5b0fa55f61d7a
yimik <398075986@163.com>
2017-06-06 00:30:39 +0800

    compress css

--- library/scss/config.rb
+++ library/scss/config.rb
@@ -19,10 +19,10 @@ relative_assets = true
 
 
 # 3. You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded
+# output_style = :expanded
 
 # 4. When you are ready to launch your WP theme comment out (3) and uncomment the line below
-# output_style = :compressed
+output_style = :compressed
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
