e438fc0081b2d5f4b65b75a394e757634a70592a
Yimik <398075986@163.com>
2017-07-01 11:00:53 +0800

    change sub-menu opacity; fix 404 mobile page

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 
     <div id="inner-content" class="wrap cf">
 
-        <main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+        <main id="main" class="m-all cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
             <article id="post-not-found" class="hentry cf mdui-shadow-1 mdui-hoverable">
                 <header class="article-header">
                     <b>4</b><b>0</b><b>4</b>
